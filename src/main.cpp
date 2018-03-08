6f89a4340a2a7ab902c2c48127bbe64c55d559ff
Alex Smith <208308s@student.gla.ac.uk>
2018-03-08 11:55:19 +0000

    wrapper for stockfish

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <unistd.h>
 using namespace std;
 #include "board.hpp"
 #include "chess_piece.hpp"
+#include <unistd.h>
 
 
 //Stockfish Includes [TODO fix relative paths]
@@ -15,12 +17,17 @@ using namespace std;
 #include "Stockfish/src/uci.h"
 #include "Stockfish/src/syzygy/tbprobe.h"
 #include <pthread.h>
+#include <cstring>
 
 char board[8][8];
+
+char command[200];
 
 namespace PSQT {
   void init();
 }
+
+void update_command(char *new_command);
 
 int main(void) {
 
@@ -37,33 +44,41 @@ int main(void) {
   TT.resize(Options["Hash"]);
   Threads.set(Options["Threads"]);
   Search::clear(); // After threads are up
-
-  char * argsa[] = {" ", "ucinewgame"};
-  UCI::loop(2, argsa);
-  char * argsb[] = {" ", "position", "startpos", "moves", "e2e4"};
-  UCI::loop(5,argsb);
-  char * argsc[] = {" ", "go"};
-  UCI::loop(2, argsc);
-
-  Threads.set(0);
   
-  cout << "GhostChess" << endl;
-    Board test;
-    cout<< test.board_to_FEN(WHITE_) << endl;
+  cout << "GhostChess V_0.1" << endl;
+  Board test;
 
-    // std::stringstream stream;
-    //test.move_piece(1,0, 2,0);
+  //char * argsb[] = {" ","position","startpos", "moves", "e2e4", "d7d5"};
+  //UCI::loop(6, argsb);
+  //update_command("uci");
+  cout << "GLOB COMMAND -> " << endl;
 
-    //test.print_board();
+  char * argse[] = {" ", "uci"};
+  UCI::loop(2, argse); // GO
+  Threads.set(0); // this must come after moves command
 
-    test.move_piece(0,0, 2,1);
-    //test.print_board();
-    cout<< test.board_to_FEN(BLACK_) << endl;
 
-    Rook rook1 = Rook(WHITE_);
-    cout << rook1.valid_move(position{7,3},position{1,3}) << endl;
-    cout << rook1.piece_colour << endl;
+  // std::stringstream stream;
+  //test.move_piece(1,0, 2,0);
 
-    return(0);
+  //test.print_board();
 
+ // test.move_piece(0,0, 2,1);
+  //test.print_board();
+ // cout<< test.board_to_FEN(BLACK_) << endl;
+
+  //Rook rook1 = Rook(WHITE_);
+  //cout << rook1.valid_move(position{7,3},position{1,3}) << endl;
+  //cout << rook1.piece_colour << endl;
+
+  return(0);
+
+}
+
+
+void update_command(char *new_command)
+{
+    size_t maxlen = strlen(new_command);
+    cout << "maxlen -> "<< maxlen << endl;
+    strcpy(command, new_command);
 }
