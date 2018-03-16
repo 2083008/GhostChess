d9b7d720b8be773a2d301f9a281f0d8774690b75
Alex Smith <208308s@student.gla.ac.uk>
2018-03-16 16:25:04 +0000

    tidied up engine init and added comments for how to use the engine

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #include "chess_piece.hpp"
 #include <unistd.h>
 
-
 //Stockfish Includes [TODO fix relative paths]
 #include "Stockfish/src/bitboard.h"
 #include "Stockfish/src/position.h"
@@ -19,6 +18,7 @@ using namespace std;
 #include <pthread.h>
 #include <cstring>
 
+// QT includes
 #include <QApplication>
 #include <QPushButton>
 #include <QPixmap>
@@ -27,87 +27,62 @@ using namespace std;
 
 char board[8][8];
 
-
-
-namespace PSQT {
-  void init();
+namespace PSQT
+{
+void init();
 }
 
 void setUpPawn(QWidget window, QString pname, QString asset_path);
+void stockfishInit();
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-  std::cout << engine_info() << std::endl;
+    // Initialise the Stockfish chess engine
+    stockfishInit();
 
-  UCI::init(Options);
-  PSQT::init();
-  Bitboards::init();
-  Position::init();
-  Bitbases::init();
-  Search::init();
-  Pawns::init();
-  Tablebases::init(Options["SyzygyPath"]);
-  TT.resize(Options["Hash"]);
-  Threads.set(Options["Threads"]);
-  Search::clear(); // After threads are up
-  
-  cout << "GhostChess V_0.1" << endl;
-  Board test;
-  const char* moves = "d7d5";
-  char setup[100] = "position startpos moves e2e4 ";
-  const char* argse[4] = {"uci", strcat(setup,moves),"isready","go"};
-  /*
-  argse[0] = "uci";
-  argse[1] = strcat(setup,moves);
-  argse[2] = "isready";
-  argse[3] = "go"; */
-  UCI::loop(4, argse); // GO
-  
-  
-  Threads.set(0); // this must come after moves command
-  
-  QApplication myapp(argc,argv);
+    cout << "GhostChess V_0.1" << endl;
 
-  QWidget window;
-  window.setFixedSize(800, 500);
-  QString assets_path = QCoreApplication::applicationDirPath() + "/src/assets/";
+    Board test;
+    // Setup engine arguments, moves holds the moves, therefore state of board
+    const char *moves = " e2e4 d7d5 e4e5 d5d4";
+    char setup[100] = "position startpos moves";
+    const char *argse[4] = {"uci", strcat(setup, moves), "isready", "go"};
 
-  QLabel *board = new QLabel("Hello world", &window);
-  //button->setGeometry(10, 10, 200, 200);
-  QPixmap pix(assets_path + "chess_board.png");
-  board->setPixmap(pix);
-  
-  //setUpPawn(window, "p1", assets_path);
+    // Run the engine
+    UCI::loop(4, argse);
 
-  /*
-  QLabel *pawn = new QLabel("p1", &window);
-  QPixmap pixp(assets_path + "pawn_white.png");
-  pawn->setPixmap(pixp);
-  pawn->setGeometry(10,320,50,80); //x1,y1,
-  */
+    QApplication myapp(argc, argv);
 
-  window.show();
-  
-  return myapp.exec();
-  
-  
-  // std::stringstream stream;
-  //test.move_piece(1,0, 2,0);
+    QWidget window;
+    window.setFixedSize(800, 500);
+    QString assets_path = QCoreApplication::applicationDirPath() + "/src/assets/";
 
-  //test.print_board();
+    QLabel *board = new QLabel("Hello world", &window);
+    //button->setGeometry(10, 10, 200, 200);
+    QPixmap pix(assets_path + "chess_board.png");
+    board->setPixmap(pix);
 
- // test.move_piece(0,0, 2,1);
-  //test.print_board();
- // cout<< test.board_to_FEN(BLACK_) << endl;
+    //setUpPawn(window, "p1", assets_path);
+    window.show();
 
-  //Rook rook1 = Rook(WHITE_);
-  //cout << rook1.valid_move(position{7,3},position{1,3}) << endl;
-  //cout << rook1.piece_colour << endl;
-
-  //return(0);
-
+    return myapp.exec();
 }
-
+void stockfishInit()
+{
+    //std::cout << engine_info() << std::endl;
+    UCI::init(Options);
+    PSQT::init();
+    Bitboards::init();
+    Position::init();
+    Bitbases::init();
+    Search::init();
+    Pawns::init();
+    Tablebases::init(Options["SyzygyPath"]);
+    TT.resize(Options["Hash"]);
+    Threads.set(Options["Threads"]);
+    Search::clear(); // After threads are up
+}
 /*
 void setUpPawn(QWidget window, QString pname, QString assets_path) {
   
