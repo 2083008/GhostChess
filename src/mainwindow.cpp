8d85bc9d666c2820b74e558907679ca17c2fe4f5
Alex Smith <208308s@student.gla.ac.uk>
2018-04-11 12:06:44 +0100

    refactored ui code from main into this MainWindow class

--- src/mainwindow.cpp
+++
@@ -1,9 +0,0 @@
-#include "mainwindow.h"
-
-Window::Window() : plot( QString("Example Plot") )
-{
-    // set up the thermometer
-    thermo.setFillBrush( QBrush(Qt::red) );
-    thermo.setRange(0, 20);
-    thermo.show();
-}
